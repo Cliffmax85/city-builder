c9be462f7a509a8d2912017678bb6dc8741b3839
cliffmax85 <cliffmax85@gmail.com>
2021-12-03 08:40:00 -0800

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,53 @@
 // import functions and grab DOM elements
+import { makeStatsString } from './utils.js';
+const skylineDropdown = document.querySelector('#skyline-dropdown');
+const castleDropdown = document.querySelector('#castle-dropdown');
+const waterfrontDropdown = document.querySelector('#waterfront-dropdown');
+const skylineImageEl = document.querySelector('#skyline-image');
+const castleImageEl = document.querySelector('#castle-image');
+const waterfrontImageEl = document.querySelector('#waterfront-image');
+const sloganInputEl = document.querySelector('#slogan-input');
+const sloganButton = document.querySelector('#slogan-button');
+const sloganListEl = document.querySelector('.list');
+const countEl = document.querySelector('.count'); 
+
+
+
 
 // let state
+let skylineCount = 0;
+let waterfrontCount = 0;
+let castleCount = 0;
+
+let sloganInput = [];
+
+
+
 
 // set event listeners 
+
+skylineDropdown.addEventListener('change', () => {
+    const skyline = skylineDropdown.value;
+    skylineCount++; 
+
+    skylineImageEl.src = `./assets/${skyline}-skyline.jpg`;
+});
+
+castleDropdown.addEventListener('change', () => {
+    const castle = castleDropdown.value;
+    castleCount++; 
+
+    castleImageEl.src = `./assets/${castle}-castle.jpg`;
+});
+
+waterfrontDropdown.addEventListener('change', () => {
+    const waterfront = waterfrontDropdown.value;
+    waterfrontCount++;
+
+    waterfrontImageEl.src = `./assets/${waterfront}-waterfront.jpg`;
+});
   // get user input
+
+
   // use user input to update state 
   // update DOM to reflect the new state
